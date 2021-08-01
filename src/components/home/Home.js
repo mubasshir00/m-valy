41476317a77700abc859eece6b7aa74665adc9f2
mubasshir00 <marakib178@gmail.com>
2021-08-02 00:36:07 +0600

    again commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,48 +1,82 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
 import './Home.css'
 import imagesArray  from './data'
 import {FaChevronLeft,FaChevronRight} from 'react-icons/fa'
 
 const Home = () => {
 
+    const [images, setImages] = useState(imagesArray);
     const [index, setIndex] = useState(0);
-    const {image} = imagesArray[index];
 
-    const checkNumber = (number) =>{
-        if( number>imagesArray.length-1){
-           return 0;
-        }
-        if(number<0){
-            return imagesArray.length-1;
-        }
-        return number;
+    console.log(images);
+
+    const prevSlide = () =>{
+        setIndex((oldIndex)=>{
+            let index = oldIndex - 1;
+            if(index <0){
+                index = images.length -1 ;
+            }
+            return index;
+        })
     }
-    const prevHandler = () =>{
-        setIndex((index)=>{
-            let newIndex = index -1;
-            return checkNumber(newIndex) ;
-        });
-    };
-    const nextHandler = () =>{
-        setIndex((index)=>{
-            let newIndex = index +1;
-            return checkNumber(newIndex);
-        });
-    };
+    const nextSlide = () =>{
+        setIndex((oldIndex)=>{
+            let index = oldIndex +1;
+            if(index>=images.length){
+                index = 0;
+            }
+            return index ;
+        })
+    }
+
+    useEffect(() => {
+        let slider = setInterval(()=>{
+            setIndex((oldIndex)=>{
+                let index = oldIndex +1;
+                if(index>images.length-1){
+                    index=0;
+                }
+                return index;
+            })
+        },5000)
+        return () => {
+            clearInterval(slider)
+        }
+    }, [index])
 
     return (
         <div className="home">
             <div className="homeContainer">
-                <img className="bannerImg" src={image} alt=""/>
-                <div className="buttonContainer">
-                <button className="prev-btn" onClick={prevHandler}>
-                <FaChevronLeft />
-                </button>
-                <button className="next-btn" onClick={nextHandler}>
+            {
+                images.map((item,itemIndex)=>{
+                    const{id,image} =item;
+
+                    let position = "nextSlide";
+                    if(itemIndex === index){
+                        position = "activeSlide"
+                    }
+                    if(itemIndex === index-1 || (index === 0 && itemIndex === images.length-1)){
+                        position = "lastSlide"
+                    }
+
+                    return (
+                        <article className={position}>
+                            <img src={image} alt="" className="bannerImg"/>
+                        </article>
+                    )
+                })
+            }
+           <div className="btnContainer">
+           <button className="prev" onClick={prevSlide}>
+                <FaChevronLeft/>
+            </button>
+            <button className="next" onClick={nextSlide}>
                 <FaChevronRight/>
-                </button>  
-                </div>
-            </div>
+            </button>
+           </div>
+
+            </div>           
         </div>
     )
 }
